78c61183b21b4570f5ac71ab07c26a185727a16e
Philipp Naderer-Puiu <philipp@naderer-puiu.at>
2017-10-08 21:03:27 +0200

    OpenSSL got more verbose => drop output

--- checkCert.sh
+++ checkCert.sh
@@ -1,4 +1,4 @@
-if true | openssl s_client -connect $1:443 -servername $1 2>/dev/null | openssl x509 -noout -checkend $2; then
+if true | openssl s_client -connect $1:443 -servername $1 2>/dev/null | openssl x509 -noout -checkend $2 > /dev/null 2>&1; then
   echo "ok"
 else
   echo "fail"
